0d360c4847e8d01bd8076fd3cb380a949c24a4dd
Liber <llb0536@gmail.com>
2011-08-06 18:22:48 +0800

    cucumber test

---
+++ spec/requests/registers_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Registeration" do
+  describe "GET /signup" do
+    it "works! (now write some real specs)" do
+      get signup_path
+      response.status.should be(200)
+    end
+  end
+end
